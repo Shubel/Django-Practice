3dca65b9a2592c76ae88552333f8adb9311188d4
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2022-11-10 11:55:21 +0600

    List item file

---
+++ list.py
@@ -0,0 +1,18 @@
+# List item
+
+myList = []
+
+myList.append('Shubel')
+myList.append('Islam')
+myList.append('Sunny')
+myList.append(24)
+myList.append(11)
+
+print(myList[0])
+print(myList[1])
+print(myList[2])
+
+print(myList[0] + ' ' + myList[1] + ' ' + myList[2])
+
+print(myList[3])
+print(myList[4])
