fb597f6a9fd2f55ece15537a95ba1b8668a6185d
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2022-11-11 11:07:04 +0600

    all list item added

--- list.py
+++ list.py
@@ -1,44 +1,44 @@
-# List item
+#List item
 
-# myList = []
+myList = []
 
-# myList.append('Shubel')
-# myList.append('Islam')
-# myList.append('Sunny')
-# myList.append(24)
-# myList.append(11)
+myList.append('Shubel')
+myList.append('Islam')
+myList.append('Sunny')
+myList.append(24)
+myList.append(11)
 
-# print(myList[0])
-# print(myList[1])
-# print(myList[2])
+print(myList[0])
+print(myList[1])
+print(myList[2])
 
-# print(myList[0] + ' ' + myList[1] + ' ' + myList[2])
+print(myList[0] + ' ' + myList[1] + ' ' + myList[2])
 
-# print(myList[3])
-# print(myList[4])
+print(myList[3])
+print(myList[4])
 
-# # using for loop
-# print("Using for loop")
-# for x in myList:
-#     print(x)
+# using for loop
+print("Using for loop")
+for x in myList:
+    print(x)
     
     
-# ### 3 list
-# Bangladesh = ['Mango', 'Banana', 'Kathal']
-# India = ['Egg', 'Milk', 'Doi']
-# Katar = ['Date-fruit', 'kaschi', 'Biriyani']
+### 3 list
+Bangladesh = ['Mango', 'Banana', 'Kathal']
+India = ['Egg', 'Milk', 'Doi']
+Katar = ['Date-fruit', 'kaschi', 'Biriyani']
 
-# print("Please enter a food item: ")
-# x = input()
+print("Please enter a food item: ")
+x = input()
 
-# if x in Bangladesh:
-#     print(f'{x} comes from Bangladesh')
-# elif x in India:
-#     print(f'{x} comes from India')
-# elif x in Katar:
-#     print(f'{x} comes from Katar')    
-# else:
-#     print('I do not know')    
+if x in Bangladesh:
+    print(f'{x} comes from Bangladesh')
+elif x in India:
+    print(f'{x} comes from India')
+elif x in Katar:
+    print(f'{x} comes from Katar')    
+else:
+    print('I do not know')    
     
 
 
