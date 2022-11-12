1345b30633de3ee6ed1b3962bf56e1c569880d55
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2022-11-12 15:24:12 +0600

    string formatting example added

--- string-formatting.py
+++ string-formatting.py
@@ -1,6 +1,13 @@
 # This prints out "John is 23 years old."
-
 name = 'John'
 age = 23
 
 print('%s is %d years old' %(name, age))
+
+
+
+
+# This prints out: A list: [1, 2, 3]
+myList = [1,2,3]
+
+print("A list: %s" % myList)
