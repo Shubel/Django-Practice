d627f521defc32fed6f53f95d889d0c3ed36d855
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2022-11-19 12:30:13 +0600

    range functin added

--- list.py
+++ list.py
@@ -132,3 +132,14 @@ print('C#' not in subjects)
 
 subjects.clear()
 print(subjects)
+
+
+###range function
+num = list(range(10))
+print(num)
+print(num[2])
+
+num = list(range(5, 11))
+print(num)
+num = list(range(5, 11, 2))
+print(num)
